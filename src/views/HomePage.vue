52e32228ff6f83680261972f8b70fabb1d5cbd1d
Owen Tsai <owentsai.v@gmail.com>
2023-03-08 15:11:30 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,6 @@
     class="w-96 mx-auto p-8 mt-10 bg-gray-100 rounded-lg flex flex-col items-center justify-center"
   >
     <h1 class="text-blue-600 text-2xl font-bold my-6">Hello World</h1>
-    <a-button type="primary">Button</a-button>
+    <a-button type="primary">Click Me</a-button>
   </div>
 </template>
